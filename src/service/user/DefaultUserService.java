db4345ce4c69ac8e7fc1c0370fe0184ee8aa5843
Melis Olmez <olmezmelis204@gmail.com>
2024-07-05 22:06:04 +0300

    update

--- src/service/user/DefaultUserService.java
+++ src/service/user/DefaultUserService.java
@@ -6,37 +6,53 @@ import model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
 
 public class DefaultUserService implements UserService {
     @Override
     public void register(User user) throws SQLException {
-        Connection connection=null;
-        PreparedStatement pstmt=null;
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet resultSet = null;
 
-        try{
-            connection= DatabaseConnection.getConnection();
-            String sql= "insert into user (name,surname,tc,tel) values (?,?,?,?) ";
-            pstmt= connection.prepareStatement(sql);
-            pstmt.setString(1,user.getName());
-            pstmt.setString(2,user.getSurname());
-            pstmt.setString(3,user.getTC());
-            pstmt.setString(4,user.getTel());
-            int row= pstmt.executeUpdate();
+        try {
+            connection = DatabaseConnection.getConnection();
 
-            if(row>0) {
+            String checkSql = "SELECT COUNT(*) FROM user WHERE tc = ?";
+            pstmt = connection.prepareStatement(checkSql);
+            pstmt.setString(1, user.getTC());
+            resultSet = pstmt.executeQuery();
+
+            if (resultSet.next() && resultSet.getInt(1) > 0) {
+                System.out.println("Bu TC numarası ile kayıt zaten mevcut!");
+                return;
+            }
+
+            String sql = "INSERT INTO user (name, surname, tc, tel) VALUES (?, ?, ?, ?)";
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getSurname());
+            pstmt.setString(3, user.getTC());
+            pstmt.setString(4, user.getTel());
+            int row = pstmt.executeUpdate();
+
+            if (row > 0) {
                 System.out.println("Kişi başarıyla kaydedildi");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(pstmt!=null){
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (pstmt != null) {
                 pstmt.close();
             }
-            if(connection!=null){
+            if (connection != null) {
                 DatabaseConnection.closeConnection(connection);
             }
         }
     }
 }
+
